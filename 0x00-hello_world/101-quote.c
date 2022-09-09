90b1a0fbf222a8400eb5a5e1d9185aca361b1da3
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-09 15:56:43 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * Description: Prints "and that piece of art is useful.." without puts
  * Return: 1
  */
-
 int main(void)
-{	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 59;
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        long 1 = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
