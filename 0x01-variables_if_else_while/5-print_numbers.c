2b200e3f7a58cfa160b0bfa14a7377811eebcde5
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-10 10:08:17 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main -Entry point
+ * Description :A program that prints all single digit numbers
+ * Return 0 always (success)
+ */
+
+int main(Void)
+
+{
+printf("0123456789\n");
+Return (0);
+
+}
