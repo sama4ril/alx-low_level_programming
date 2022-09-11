7e3193f3966b26a5ebffc03fa6b71cfc7a849e22
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-11 07:16:31 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,7 @@ for (i = 10; i < 20; i++)
 putchar((i % 10) + '0');
 if (i != 19)
 {
-putchar(',');																			putchar(' ');
-}
+putchar(',');																			}
 }
 putchar('\n');
 return (0);
