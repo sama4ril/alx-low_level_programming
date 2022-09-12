416c7e7fb25b09cf978bf1d47ead954172c6b07e
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-12 11:29:03 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * main - entry point
- * Return: alway returns 0
- */
+/** 
+ * main - Entry point
+ * Hexadecimal Number
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-{
-	unsigned char a = '0';
-	int i;
-
-	for (i = 0; i < 10; i++)
 	{	
-	putchar(a);	
-	 a++;
-	}
-	a = '1';
-	for (i = 0; i < 6; i++)
-	{
-	putchar('0' + a);
-	a++;
-	}
-	putchar ('\n');
-	return (0);
+	int d;
+	char low;
+		for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		for (low = 'a'; low <= 'f'; low++)						putchar(low);
+		putchar('\n');
+		return (0);
 }
