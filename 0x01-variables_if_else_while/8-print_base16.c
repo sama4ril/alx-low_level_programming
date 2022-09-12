70603d595db093be6192dfbbc4bc496bb7bcf287
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-12 02:53:07 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,17 +10,17 @@ int main(void)
 	unsigned char a = '0';
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for(i = 0; i < 10; i++)
 	{	
 		putchar(a);	
 		a++;
 	}
 	a = '1';
-	for (i = 0; i < 6; i++)
+	for(i = 0; i < 6; i++)
 	{
 		putchar('0' + a);
 		a++;
 	}
 	putchar('\n');
-	return (0);
+	return(0);
 }
