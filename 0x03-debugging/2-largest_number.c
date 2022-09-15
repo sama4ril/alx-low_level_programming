73a176b3a6d556d7c89055c436a179b646e9f52d
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-09-15 05:15:42 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,18 +13,16 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a >= b && a >= c)
-
 	{
 		largest = a;
 	}
-		else if (b >= a && b >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-		else
+	else
 	{
 		largest = c;
 	}
-
         return (largest);
 }
