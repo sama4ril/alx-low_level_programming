dff27d76e0ee897e578bc90151ea21761e39d7fe
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-10-03 12:14:10 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-*_putchar - writes the character c to stdout
-*@c: The character to print
-*Return: On success 1.
-*On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
